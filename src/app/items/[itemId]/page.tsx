81c48e5ce4b417613009eebd02fba1a89d477d9d
hwangseunghyun <hwamg123@naver.com>
2025-09-01 14:41:22 +0900

    feat: 초기셋팅 , 페이지 , 기본이미지

---
+++ src/app/items/[itemId]/page.tsx
@@ -0,0 +1,12 @@
+export default function page() {
+  return (
+    <div className="bg-white">하이루방구
+      <button
+        className="bg-slate-200 text-slate-900 rounded-3xl font-semibold border-2 border-slate-900 h-14 min-w-14 shadow-[2px_2px_0px_#0F172A]"
+      >
+        +
+      </button>
+    </div>
+  )
+}
+
---
+++ tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+        screens: {
+            mobile: "375px",  
+            tablet: "744px",  
+            pc: "1024px",  
+          },
+      colors: {
+        slate: {
+          900: "#0F172A",
+          800: "#1E293B",
+          500: "#64748B",
+          400: "#94A3B8",
+          300: "#CBD5E1",
+          200: "#E2E8F0",
+          100: "#F1F5F9",
+        },
+        violet: {
+          600: "#7C3AED",
+          100: "#EDE9FE",
+        },
+        rose: {
+          500: "#F43F5E",
+        },
+        lime: {
+          300: "#BEF264",
+        },
+        amber: {
+          800: "#92400E",
+        },
+
+      },
+
+    },
+  },
+};
+
+export default config;
